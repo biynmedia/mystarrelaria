dfb20d69c3a7c4904c59875b499123dac4f47df7
GuillaumeVetu <32734978+GuillaumeVetu@users.noreply.github.com>
2018-01-17 16:45:47 +0100

    Travail du 17/01/2018
    -Ajout de la connexion à la bdd de Doctrine et Idiorm.
    -Création du dossier Model et du fichier Artiste.php
    -Html de profil.html.twig mis en place.
    -Html de artiste.html.twig importer.
    ->Besoin d'aide demain si possible.

--- Silex/ressources/config/database.config.php
+++ Silex/ressources/config/database.config.php
@@ -12,21 +12,26 @@ define('DBPASSWORD', '');
 $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
     'db.options' => array(
         'driver'    => 'pdo_mysql',
-        'host'      => DBHOST,
-        'dbname'    => DBNAME,
-        'user'      => DBUSERNAME,
-        'password'  => DBPASSWORD
+        'host'      => 'localhost',
+        'dbname'    => 'Relaria',
+        'user'      => 'root',
+        'password'  => ''
     ),
 ));
 
 #3 : Idiorm ORM
 $app->register(new IdiormServiceProvider(), array(
     'idiorm.db.options' => array(
-        'connection_string' => 'mysql:host='.DBHOST.';dbname='.DBNAME,
-        'username' => DBUSERNAME,
-        'password' => DBPASSWORD,
+        'connection_string' => 'mysql:host=localhost;dbname=Relaria',
+        'username' => 'root',
+        'password' => '',
         'id_column_overrides' => array(
             'genre' => 'IDGENRE'
         )
     )
 ));
+
+# 6.1 Récupération des Artistes
+$app['artiste'] = function() use($app) {
+    return $app['db']->fetchAll('SELECT * FROM artiste');
+};
