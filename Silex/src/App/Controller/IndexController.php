f116d3408c9df3b3d69bc9450d54f10fc81ebffd
Alexis Dussart <32734984+alexisdsrt@users.noreply.github.com>
2018-01-24 08:56:14 +0100

    23/01/18
    - TODO SERIALIZE

--- Silex/src/App/Controller/IndexController.php
+++ Silex/src/App/Controller/IndexController.php
@@ -58,7 +58,7 @@ class IndexController
                     $membre->EMAILMEMBRE    = $email = $request->get('EMAILMEMBRE');
                     $membre->MDPMEMBRE      = $app['security.default_encoder']
                         ->encodePassword($request->get('MDPMEMBRE'), '');
-                    $membre->ROLEMEMBRE     = serialize(['ROLE_MEMBRE']);
+                    $membre->ROLEMEMBRE     = serialize(['ROLE_MEMBRE']); // TODO : Serialize des Roles
 
                     # On persiste en BDD
                     $membre->save();
